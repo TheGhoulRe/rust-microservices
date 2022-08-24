bfc984b3956658e5696ac84eee9ab27e41167639
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-08-24 12:10:23 +0100

    final checkpoint

--- kafka-consumer/src/main.rs
+++ kafka-consumer/src/main.rs
@@ -1,18 +1,21 @@
 use kafka::consumer::{Consumer, FetchOffset};
-use std::str::from_utf8;
+use std::str;
 
 fn main () {
+  // Create a consumer for the "topic-name" topic
   let mut consumer =
     Consumer::from_hosts(vec!("localhost:9092".to_owned()))
-      .with_topic_partitions("my-topic".to_owned(), &[0])
+      .with_topic_partitions("topic-name".to_owned(), &[0])
       .with_fallback_offset(FetchOffset::Latest)
       .create()
       .unwrap();
   
+  // Create a listener to listen to events
   loop {
     for ms in consumer.poll().unwrap().iter() {
       for m in ms.messages() {
-        println!("{:?}", from_utf8(m.value).unwrap());
+        // convert message in variable m from bytes to string
+        println!("{:?}", str::from_utf8(m.value).unwrap());
       }
 
       consumer.consume_messageset(ms).unwrap();
--- kafka-producer/src/main.rs
+++ kafka-producer/src/main.rs
@@ -1,16 +1,15 @@
-use std::time::Duration;
-use kafka::producer::{Producer, Record, RequiredAcks};
+use kafka::producer::{Producer, Record};
 
 fn main() {
+  // create the producer
   let mut producer =
     Producer::from_hosts(vec!("localhost:9092".to_owned()))
-      .with_ack_timeout(Duration::from_secs(1))
-      .with_required_acks(RequiredAcks::One)
       .create()
       .unwrap();
   
+  // send a numbers 0 through 9 to the topic named "topic-name"
   for i in 0..10 {
-    let buf = format!("[{i}]");
+    let buf = format!("{i}");
     producer.send(&Record::from_value("my-topic", buf.as_bytes())).unwrap();
     println!("Sent: {i}");
   }

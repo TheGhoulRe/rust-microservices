57d786ef90ec6a3ad7a451d1888cf177c96cfd89
Chigozie Oduah <oduahchigozie46@gmail.com>
2022-08-26 12:01:33 +0100

    refactored main.rs

--- texts-microservice/src/main.rs
+++ texts-microservice/src/main.rs
@@ -1,4 +1,4 @@
-use kafka::consumer::{Consumer, FetchOffset};
+use kafka::consumer::{Consumer, FetchOffset, Message};
 use kafka::producer::{Producer, Record};
 use std::str;
 use serde_json::{json, Value};
@@ -30,36 +30,8 @@ fn main() {
   // Listen to events from the actions topic consumer
   loop {
     for ms in consumer.poll().unwrap().iter() {
-      for m in ms.messages() { // this block is executed if it picks up an event
-
-        // parse event from the consumer
-        let event_json = str::from_utf8(m.value).unwrap().to_owned();
-        let event: Value = serde_json::from_str(&event_json).unwrap();
-
-        if event["action"] == "add" {
-
-          // add “value” from event to "texts"
-          texts.push( event["value"].to_string() );
-          
-          // publish list to the "texts" topic
-          let json_bytes = json!(&texts).to_string();
-          let record = Record::from_value("texts", json_bytes.as_bytes() );
-          producer.send(&record).unwrap();
-
-        } else if event["action"] == "remove" {
-
-          // use "index" to remove item from "texts"
-          texts.remove( event["value"].to_string().parse::<usize>().unwrap() );
-
-          // publish list to the "texts" topic
-          let json_bytes = json!(&texts).to_string();
-          let record = Record::from_value("texts", json_bytes.as_bytes() );
-          producer.send(&record).unwrap();
-
-        } else {
-          println!("Invalid action");
-        }
-
+      for m in ms.messages() {
+        consume_event(m, &mut texts, &mut producer);
       }
 
       consumer.consume_messageset(ms).unwrap();
@@ -67,4 +39,60 @@ fn main() {
 
     consumer.commit_consumed().unwrap();
   }
+}
+
+fn consume_event( m: &Message, texts: &mut Vec<String>, producer: &mut Producer ) {
+
+  // convert event message from bytes to string
+  let event_json = str::from_utf8(m.value).unwrap().to_owned();
+
+  // parse event from the consumer
+  let event: Value = serde_json::from_str(&event_json).unwrap();
+
+  // get action to perform from event
+  let action: String = event["action"].to_string();
+
+  if action == "add" {
+    add_text(&event, texts, producer);
+  } else if action == "remove" {
+    remove_text(&event, texts, producer);
+  } else {
+    println!("Invalid action");
+  }
+}
+
+fn add_text(event: &Value, texts: &mut Vec<String>, producer: &mut Producer) {
+
+  // get text from event JSON
+  let text = event["value"].to_string();
+
+  // add text to list
+  texts.push(text);
+    
+  // convert list to JSON
+  let json_bytes = json!(&texts).to_string();
+
+  // prepare for publishing
+  let record = Record::from_value("texts", json_bytes.as_bytes() );
+
+  // publish to broker
+  producer.send(&record).unwrap();
+}
+
+fn remove_text(event: &Value, texts: &mut Vec<String>, producer: &mut Producer) {
+
+  // get index of text to remove
+  let index = event["value"].to_string().parse::<usize>().unwrap();
+
+  // remove text on specified index
+  texts.remove( index );
+
+  // convert list to JSON
+  let json_bytes = json!(&texts).to_string();
+
+  // prepare for publishing
+  let record = Record::from_value("texts", json_bytes.as_bytes() );
+
+  // publish to broker
+  producer.send(&record).unwrap();
 }
